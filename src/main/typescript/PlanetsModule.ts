559dfa0b3c039dc45c131e6e6b27e889bfa1ba32
Benoit Lemoine <blemoine@xebia.fr>
2014-03-08 14:58:29 +0100

    Mise en place controller + getter

--- src/main/typescript/PlanetsModule.ts
+++ src/main/typescript/PlanetsModule.ts
@@ -1,14 +1,22 @@
 /// <reference path="../definition/angularjs/angular.d.ts" />
 
-var planetsModule = angular.module('planetsModule',[]);
+var planetsModule = angular.module('planetsModule', []);
 
 class PlanetsController {
 
-    planets:Array<String> = [];
+    filter:string = null;
+    planets:Array<string> = [];
 
     constructor() {
-        this.planets = ['mercure','venus','terre','mars','jupiter','saturne','uranus','neptune'];
+        this.planets = ['mercure', 'venus', 'terre', 'mars', 'jupiter', 'saturne', 'uranus', 'neptune'];
+    }
+
+    get planetsFiltered() {
+        if (this.filter) {
+            return this.planets.filter((planet) => planet.indexOf(this.filter) >= 0)
+        }
+        return this.planets;
     }
 }
 
-planetsModule.controller('PlanetsController',PlanetsController);
+planetsModule.controller('PlanetsController', PlanetsController);
